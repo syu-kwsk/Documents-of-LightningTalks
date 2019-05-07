280bf925c58ec6d4a2767b23d3c356fb9e5b678a
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2019-05-07 15:02:09 +0900

    delete random

--- LT-1/assets/sleep.py
+++ LT-1/assets/sleep.py
@@ -1,6 +1,9 @@
-import random
-from time import sleep
+from time import sleep #sleep関数だけimport
 
-def thinkingTimer(time):
+#関数定義
+def thinkingTimer(time):　
     print('. . .\n')
     sleep(time)
+
+
+    
